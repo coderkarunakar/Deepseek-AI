6ab2eb6232577700830af05af237734a1f9c49e2
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2025-04-24 20:10:09 +0530

    Backend Server is Ready

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+import express from "express";
+import dotenv from 'dotenv'
+ 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port =  4001;
 
-app.get("/",(req,res)=>{
-    res.send("helllo");
-});
+
 app.listen(port, ()=>{
-    console.log(`Example app is listening on port ${port}`);
+    console.log(` app is listening on port ${port}`);
 });
