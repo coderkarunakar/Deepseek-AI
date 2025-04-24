fe20dc5a363d138c1483533cdef69ce8dc45dbff
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2025-04-24 20:27:49 +0530

    connecte dot Database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from 'dotenv'
- 
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 const port =  4001;
-
+const MONGO_URI = process.env.MONGODB_URI
+//Db connection Code goes here
+mongoose.connect(MONGO_URI).then(() =>{
+    console.log("Connected to Mongodb")
+}).catch((err)=>{
+    console.error("MongoDB connection Error",error)
+})
 
 app.listen(port, ()=>{
     console.log(` app is listening on port ${port}`);
